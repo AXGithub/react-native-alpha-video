879b5897486fe3e727233119e467a74842354abb
jed <jedshim@163.com>
2021-04-15 18:21:18 +0800

    安卓修改页面

--- android/src/main/java/com/reactlibrary/AlphaVideoView.java
+++ android/src/main/java/com/reactlibrary/AlphaVideoView.java
@@ -1,37 +1,53 @@
 package com.reactlibrary;
 
 import android.app.Activity;
-import android.content.Context;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactContext;
 import com.reactlibrary.mxVideo.MxVideoView;
 
 public class AlphaVideoView extends FrameLayout {
     private static final String TAG = "AlphaVideoView";
-    private final Activity activity;
-    private final ReactContext context;
+    private final Activity _activity;
+    private final ReactContext _context;
 
     public MxVideoView videoView;
 
     public AlphaVideoView(Activity activity, @NonNull ReactContext context) {
         super(context);
-        this.activity = activity;
-        this.context = context;
-
-        Log.d(TAG, "AlphaVideoView: init -- ");
+        this._activity = activity;
+        this._context = context;
         videoView = new MxVideoView(context, null);
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
-        activity.addContentView(videoView, layoutParams);
+
+        initView();
+    }
+
+    private void initView() {
+
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        videoView.setLayoutParams(layoutParams);
+        this.addView(videoView);
+
+        videoView.setOnVideoEndedListener(new MxVideoView.OnVideoEndedListener() {
+            @Override
+            public void onVideoEnded() {
+                Log.d(TAG, "onVideoEnded: 播放完成");
+                videoView.release();
+                closeView();
+            }
+        });
 
         videoView.setVideoFromAssets("aa.mp4");
-        Log.d(TAG, "AlphaVideoView: 播放了aa.mp4");
     }
+
+    private void closeView() {
+        this.removeView(videoView);
+    }
+
+
 }
